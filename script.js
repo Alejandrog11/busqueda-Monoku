6f5803f33dd5daa1f93635fc844a71e962bb757b
Elizabeth Peña Tejeda <Eli@MacBook-Pro-de-Elizabeth.local>
2016-08-31 13:57:22 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+var options = {}
+options[last.fm.search.Search.RESTRICT_EXTENDED_ARGS] =
+  {'as_sitesearch' : 'last.fm/music_search/'};
+var busquedaper = new .search.busquedaper(
+ '42f75f939105d2110d6a0daf27db431c', options);
